925a916a87458341cce8a1cf90b8f8f77042abd3
Emery <144450140+EmeryMartin@users.noreply.github.com>
2023-09-22 15:20:58 -0300

    Finished Roll-a-Ball Tutorial

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,23 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
+
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
     private Rigidbody rb;
-
+    private int count;
     private float movementX;
     private float movementY;
-
+    
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
+        winTextObject.SetActive(false);
+        //QuitButtonObject.SetActive(false);
+
     }
 
-    private void OnMove(InputValue movementValue)
+    void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
 
@@ -26,11 +36,31 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y;
     }
 
-    private void FixedUpdate()
+    void SetCountText()
+    {
+        countText.text = "Coins: " + count.ToString() + "/13";
+        if(count >= 13)
+        {
+            winTextObject.SetActive(true);
+            //QuitButtonObject.SetActive(true);
+        }
+    }
+
+    void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-
         rb.AddForce(movement * speed);
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+
+        SetCountText();
+    }
 }
