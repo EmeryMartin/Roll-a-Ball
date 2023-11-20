8956e660827396def053469c021394d07eef5936
Emery <144450140+EmeryMartin@users.noreply.github.com>
2023-11-20 14:47:05 -0400

    added collectables
    added collectable market items, PickUp controller script

---
+++ Assets/Scripts/PickUpController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using TMPro;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    private Rigidbody rb;
+    private int count;
+    public TextMeshProUGUI countText;
+    public GameObject guideTextObject;
+    public GameObject countTextObject;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
+
+
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Groceries Collected: " + count.ToString() + "/6";
+        if (count >= 6)
+        {
+            guideTextObject.SetActive(true);
+            countTextObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+
+        SetCountText();
+    }
+}
